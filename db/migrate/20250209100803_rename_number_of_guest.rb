6704d0e62433e768c5ec190a779bdd819a6cf3a4
joey collier <joey.collier.pro@gmail.com>
2025-02-09 21:40:51 +0100

    Rails-Simple-Airbnb

---
+++ db/migrate/20250209100803_rename_number_of_guest.rb
@@ -0,0 +1,5 @@
+class RenameNumberOfGuest < ActiveRecord::Migration[7.1]
+  def change
+    rename_column :flats, :number_of_guest, :number_of_guests
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Flat.create!(name: 'Stylish House Close to River Thames',
+  address: '5 Queensmill Road London SW6 6JP',
+  description: 'Beautiful house',
+  price_per_night: 65,
+  number_of_guests: 2
+)
+Flat.create!(name: 'St Pancras Clock Tower Guest Suite',
+  address: 'Euston Rd London N1C 4QP',
+  description: 'Many activities nearby this suite, wonderfull experience garanty !',
+  price_per_night: 110,
+  number_of_guests: 2
+)
